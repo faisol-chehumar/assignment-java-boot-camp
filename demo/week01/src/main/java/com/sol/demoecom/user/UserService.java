e434ba418fd398c835824a4f410fd6b8b4d53ee7
faisolchehumar <faisol.che@knstrans.com>
2022-02-24 21:29:05 +0700

    Create UserService and test #9

---
+++ demo/week01/src/main/java/com/sol/demoecom/user/UserService.java
@@ -0,0 +1,27 @@
+package com.sol.demoecom.user;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public void setUserRepository(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public String login(String username, String password) throws AuthenticationFailException {
+        Optional<UserModel> result = userRepository.findByUsernameAndPassword(username, password);
+        if(result.isPresent()) {
+            return "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6IlVzZXIwMSJ9.saNh8MedJAAeSWE06XH5M-2EcWny0ZFfvIS-qJjFfWk";
+        }
+
+        throw new AuthenticationFailException();
+    }
+}
---
+++ demo/week01/src/test/java/com/sol/demoecom/user/UserServiceTest.java
@@ -0,0 +1,41 @@
+package com.sol.demoecom.user;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+public class UserServiceTest {
+
+    @Mock
+    private UserRepository userRepository;
+
+    @Test
+    @DisplayName("Should return accessToken when login with correct username and password")
+    void login() throws AuthenticationFailException {
+        // Arrange
+        UserService userService = new UserService();
+        userService.setUserRepository(userRepository);
+
+        String username = "user01";
+        String password = "pass";
+        String accessToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6IlVzZXIwMSJ9.saNh8MedJAAeSWE06XH5M-2EcWny0ZFfvIS-qJjFfWk";
+
+        UserModel user = new UserModel(username, password);
+        when(userRepository.findByUsernameAndPassword(username, password)).thenReturn(Optional.of(user));
+
+        // Assert
+        String result = userService.login(username, password);
+
+        // Assert
+        assertThat(result).isEqualTo(accessToken);
+    }
+}
