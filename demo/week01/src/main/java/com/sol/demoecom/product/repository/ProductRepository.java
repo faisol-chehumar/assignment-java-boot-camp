6619250da2587a034f2086270c92d556d818240d
faisolchehumar <faisol.che@knstrans.com>
2022-02-26 02:20:36 +0700

    Create ProductRepository, test and Move models to model directory #17

---
+++ demo/week01/src/main/java/com/sol/demoecom/product/repository/ProductRepository.java
@@ -0,0 +1,11 @@
+package com.sol.demoecom.product.repository;
+
+import com.sol.demoecom.product.model.ProductModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface ProductRepository extends JpaRepository<ProductModel, UUID> {
+    List<ProductModel> findByNameContainsIgnoreCase(String name);
+}
