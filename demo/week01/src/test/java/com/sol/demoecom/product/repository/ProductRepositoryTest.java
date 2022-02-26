16fd1c6d1462e1e641dd49c4030a0e6ddd93f3d6
faisolchehumar <faisol.che@knstrans.com>
2022-02-26 15:40:11 +0700

    Rename method and display name in ProductRepository #17

---
+++ demo/week01/src/test/java/com/sol/demoecom/product/repository/ProductRepositoryTest.java
@@ -0,0 +1,34 @@
+package com.sol.demoecom.product.repository;
+
+import com.sol.demoecom.product.model.ProductModel;
+import com.sol.demoecom.product.repository.ProductRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataJpaTest
+public class ProductRepositoryTest {
+    @Autowired
+    private ProductRepository productRepository;
+
+    @Test
+    @DisplayName("Should return true if product size is 2 when input is 'Nike'.")
+    void findByNameContainsIgnoreCase_withInitialProduct_productSizeEqual2() {
+        // Arrange
+        ProductModel product1 = new ProductModel("Nike Yeezy", "234510253456", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam imperdiet libero magna, eu volutpat metus euismod sit amet. Nullam aliquet vel orci ac efficitur.", "กรุงเทพ", 365);
+        ProductModel product2 = new ProductModel("Adidas Nike", "43181728493", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam imperdiet libero magna, eu volutpat metus euismod sit amet. Nullam aliquet vel orci ac efficitur.", "กรุงเทพ", 365);
+        productRepository.save(product1);
+        productRepository.save(product2);
+        // Act
+        List<ProductModel> products = productRepository.findByNameContainsIgnoreCase("Nike");
+
+        // Assert
+        assertThat(products).hasSize(2);
+    }
+}
