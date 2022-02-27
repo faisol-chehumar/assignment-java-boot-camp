e7b252a50cd2d51ed9cca5cb76c336d6b3a6b480
faisolchehumar <faisol.che@knstrans.com>
2022-02-27 14:30:00 +0700

    Create getMyBasket() in UserController #39

--- demo/week01/src/main/java/com/sol/demoecom/user/UserController.java
+++ demo/week01/src/main/java/com/sol/demoecom/user/UserController.java
@@ -1,24 +1,27 @@
 package com.sol.demoecom.user;
 
 import com.sol.demoecom.common.ResponseSuccess;
-import com.sol.demoecom.product.model.ProductSkuModel;
+import com.sol.demoecom.product.controller.response.ProductsItem;
+import com.sol.demoecom.product.mapper.ProductItemMapper;
 import com.sol.demoecom.product.repository.ProductSkuRepository;
+import com.sol.demoecom.user.mapper.BasketItemMapper;
 import com.sol.demoecom.user.model.UserBasketItemModel;
 import com.sol.demoecom.user.model.UserBasketModel;
 import com.sol.demoecom.user.model.UserModel;
+import com.sol.demoecom.user.repository.UserBasketRepository;
 import com.sol.demoecom.user.repository.UserRepository;
 import com.sol.demoecom.user.request.AddItemToBasketRequest;
 import com.sol.demoecom.user.request.LoginRequest;
+import com.sol.demoecom.user.response.BasketItems;
+import com.sol.demoecom.user.response.MyBasket;
 import com.sol.demoecom.user.response.UserCredential;
 import com.sol.demoecom.user.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.Optional;
+import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @RestController
 public class UserController {
@@ -28,6 +31,9 @@ public class UserController {
 
     @Autowired
     private UserRepository userRepository;
+
+    @Autowired
+    private UserBasketRepository userBasketRepository;
 
     @Autowired
     private ProductSkuRepository productSkuRepository;
@@ -47,5 +53,16 @@ public class UserController {
 
         return new ResponseSuccess(null);
 
+    }
+
+    @GetMapping("/user/basket")
+    public ResponseSuccess<MyBasket> getMyBasket(@RequestParam UUID userId) {
+        System.out.println("getMyBasketRequest" + userId);
+        UserModel user = userRepository.getById(userId);
+        UserBasketModel userBasket = user.getUserBasket();
+        List<UserBasketItemModel> itemsInBasket = userBasket.getItems();
+        List<BasketItems> items = itemsInBasket.stream().map(i -> new BasketItemMapper().mapRow(i)).collect(Collectors.toList());
+
+        return new ResponseSuccess(new MyBasket(userBasket.getItems().size(), items));
     }
 }
--- demo/week01/src/test/java/com/sol/demoecom/user/controller/UserControllerTest.java
+++ demo/week01/src/test/java/com/sol/demoecom/user/controller/UserControllerTest.java
@@ -3,6 +3,7 @@ package com.sol.demoecom.user.controller;
 import com.sol.demoecom.product.model.ProductModel;
 import com.sol.demoecom.product.model.ProductSkuModel;
 import com.sol.demoecom.product.repository.ProductSkuRepository;
+import com.sol.demoecom.user.model.UserBasketItemModel;
 import com.sol.demoecom.user.model.UserBasketModel;
 import com.sol.demoecom.user.model.UserModel;
 import com.sol.demoecom.user.repository.UserRepository;
@@ -14,12 +15,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
+import org.springframework.http.*;
 
-import java.util.Optional;
-import java.util.UUID;
+import java.util.*;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.when;
@@ -83,9 +81,11 @@ class UserControllerTest {
         productSku.setId(productSkuUuid);
         product.addProductSku(productSku);
 
-        when(userRepository.getById(user.getId())).thenReturn(user);
+
         when(productSkuRepository.findById(productSku.getId())).thenReturn(Optional.of(productSku));
         when(userRepository.findById(user.getId())).thenReturn(Optional.of(user));
+        when(userRepository.getById(user.getId())).thenReturn(user);
+
 
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
@@ -101,6 +101,65 @@ class UserControllerTest {
         JSONObject jsonResponse = new JSONObject(stringResponse);
 
         boolean success = jsonResponse.getBoolean("success");
+
+        // Assert
+        assertThat(success).isEqualTo(true);
+    }
+
+    @Test
+    @DisplayName("Should return success true when get item list in basket.")
+    void getMyBasket_withOneProductItem_sizeEqualTo1() throws JSONException {
+        // Arrange
+        String testUsername = "user01";
+        String testPassword = "pass";
+        UserModel user = new UserModel(testUsername, testPassword);
+        UUID UserUuid = UUID.fromString("8b2b55d0-3269-4bb2-8404-b52ce4e41fef");
+        user.setId(UserUuid);
+        user.setUserBasket(new UserBasketModel(user));
+
+        ProductModel product = new ProductModel("Adidas R1 Black and Grey FY5727, FZ0077, FY5730 ", "234470233456", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam imperdiet libero magna, eu volutpat metus euismod sit amet. Nullam aliquet vel orci ac efficitur.", "กรุงเทพ", 365);
+        product.setId(UUID.fromString("4cad76d1-2bef-4125-83c3-779570dbe65d"));
+
+        ProductSkuModel productSku = new ProductSkuModel("nmd-bl-sm", "sdsd", 1000, 1000, 50, product);
+        UUID productSkuUuid = UUID.fromString("4e8b7b81-c66d-435b-a58b-0e7d3e03d7aa");
+        productSku.setId(productSkuUuid);
+        product.addProductSku(productSku);
+
+        List<UserBasketItemModel> items = new ArrayList<>();
+        UserBasketItemModel userBasketItem = new UserBasketItemModel(user.getUserBasket(), productSku, 2);
+        items.add(userBasketItem);
+        user.getUserBasket().setItems(items);
+
+        when(userRepository.findById(user.getId())).thenReturn(Optional.of(user));
+        when(userRepository.getById(user.getId())).thenReturn(user);
+
+        // create headers
+        HttpHeaders headers = new HttpHeaders();
+
+        // set `Content-Type` and `Accept` headers
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
+        // example of custom header
+        headers.set("token", user.getId().toString());
+
+        // build the request
+        HttpEntity request = new HttpEntity(headers);
+
+
+        ResponseEntity<String> stringResponse = testRestTemplate.exchange(
+                String.format("/user/basket?userId=%s", user.getId().toString()),
+                HttpMethod.GET,
+                request,
+                String.class,
+                1
+        );
+
+        // Action
+        JSONObject jsonResponse = new JSONObject(stringResponse.getBody());
+
+        boolean success = jsonResponse.getBoolean("success");
+
+        System.out.println("jsonResponse" + jsonResponse);
 
         // Assert
         assertThat(success).isEqualTo(true);
